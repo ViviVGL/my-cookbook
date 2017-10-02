68f48f34c309238a43ae3ec3ceb8aa3daf770cf7
Virginia Lusiani <virginia.lusiani@gmail.com>
2017-10-02 16:35:53 -0300

    Administrador cadastra cozinhas

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,4 +2,18 @@ class CuisinesController < ApplicationController
   def show
     @cuisine = Cuisine.find(params[:id])
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    @cuisine = Cuisine.new(params.require(:cuisine).permit(:name))
+    if @cuisine.save
+      redirect_to @cuisine
+    else
+      flash[:error] = 'Você deve informar o nome da cozinha'
+      redirect_to new_cuisine_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :recipes, only: [:show, :new, :create]
-  resources :cuisines, only: [:show]
+  resources :cuisines, only: [:show, :new, :create]
 
 end
